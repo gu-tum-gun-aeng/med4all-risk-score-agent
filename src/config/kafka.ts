ca1198ba37aa50348f4c77eba5a94812a898ad0b
yort <krittipong@ava.fund>
2021-08-11 18:30:49 +0700

    fixed lint

--- src/config/kafka.ts
+++ src/config/kafka.ts
@@ -1,12 +1,13 @@
-import { env } from "process";
-import { v4 as uuidv4 } from "uuid";
+import { env } from "process"
 
-const CLIENT_ID = `med4all-waiting-list-agent-${uuidv4()}`;
-const BROKER_LIST = env.KAFKA_BROKER_LIST?.split(",") || ["localhost:9092"];
-const GROUP_ID = env.GROUP_ID || "test-group";
+import { v4 as uuidv4 } from "uuid"
+
+const CLIENT_ID = `med4all-waiting-list-agent-${uuidv4()}`
+const BROKER_LIST = env.KAFKA_BROKER_LIST?.split(",") || ["localhost:9092"]
+const GROUP_ID = env.GROUP_ID || "test-group"
 
 export default {
   CLIENT_ID,
   BROKER_LIST,
   GROUP_ID,
-} as const;
+} as const
