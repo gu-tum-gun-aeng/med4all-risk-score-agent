20662741464c3df62d1af824510a64e212702b28
yort <krittipong@ava.fund>
2021-08-11 18:07:55 +0700

    read config from env

--- src/config/kafka.ts
+++ src/config/kafka.ts
@@ -1,11 +1,12 @@
-import { v4 as uuidv4 } from "uuid"
+import { v4 as uuidv4 } from "uuid";
+import { env } from "process";
 
-const CLIENT_ID = `med4all-waiting-list-agent-${uuidv4()}`
-const BROKER_LIST = ["localhost:9092"]
-const GROUP_ID = "test-group"
+const CLIENT_ID = `med4all-waiting-list-agent-${uuidv4()}`;
+const BROKER_LIST = env.KAFKA_BROKER_LIST?.split(",") || ["localhost:9092"];
+const GROUP_ID = env.GROUP_ID || "test-group";
 
 export default {
   CLIENT_ID,
   BROKER_LIST,
   GROUP_ID,
-} as const
+} as const;
