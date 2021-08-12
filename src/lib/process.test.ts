b41f8de55a6edbf40519fb9d28f7060801eacfba
dodadoa <dodadoa@Avas-Genesis.local>
2021-08-12 22:34:26 +0700

    refactor: add more tests on process gender code

--- src/lib/process.test.ts
+++ src/lib/process.test.ts
@@ -8,7 +8,7 @@ import {
   PatientWithRiskScore,
   RiskScoreResponse,
 } from "./model"
-import Process from "./process"
+import Process, { mapGenderCode } from "./process"
 
 export const buildPatientInfo = (): Patient => {
   return {
@@ -41,39 +41,60 @@ export const buildPatientInfo = (): Patient => {
 }
 
 describe("Process", () => {
-  test("processRiskScore should return correct PatientWithRiskScore if we put patient object with full information", async () => {
-    const expectedResultFromApi: RiskScoreResponse = {
-      inclusion_label: "R2",
-      inclusion_label_type: "normal",
-      triage_score: 136,
-    }
+  describe("processRiskScore", () => {
+    test("processRiskScore should return correct PatientWithRiskScore if we put patient object with full information", async () => {
+      const expectedResultFromApi: RiskScoreResponse = {
+        inclusion_label: "R2",
+        inclusion_label_type: "normal",
+        triage_score: 136,
+      }
 
-    const patientInfo: Patient = buildPatientInfo()
+      const patientInfo: Patient = buildPatientInfo()
 
-    const stubAxiosPost = sinon.stub(axios, "post")
-    stubAxiosPost.resolves({ data: expectedResultFromApi })
+      const stubAxiosPost = sinon.stub(axios, "post")
+      stubAxiosPost.resolves({ data: expectedResultFromApi })
 
-    const result: PatientWithRiskScore = await Process.processRiskScore(
-      patientInfo
-    )
+      const result: PatientWithRiskScore = await Process.processRiskScore(
+        patientInfo
+      )
 
-    const expected = {
-      ...patientInfo,
-      riskScore: expectedResultFromApi,
-    }
-    expect(result).toEqual(expected)
+      const expected = {
+        ...patientInfo,
+        riskScore: expectedResultFromApi,
+      }
+      expect(result).toEqual(expected)
 
-    stubAxiosPost.restore()
+      stubAxiosPost.restore()
+    })
+
+    test("processRiskScore should throw error if no patient data of age", async () => {
+      const patientInfo = buildPatientInfo()
+      const mockPatientWithoutAge: Omit<Patient, "cdPersonAge"> = _.omit(
+        patientInfo,
+        "cdPersonAge"
+      )
+      expect(Process.processRiskScore(mockPatientWithoutAge)).rejects.toEqual(
+        new Error("no cd person age")
+      )
+    })
   })
 
-  test("processRiskScore should throw error if no patient data of age", async () => {
-    const patientInfo = buildPatientInfo()
-    const mockPatientWithoutAge: Omit<Patient, "cdPersonAge"> = _.omit(
-      patientInfo,
-      "cdPersonAge"
-    )
-    expect(Process.processRiskScore(mockPatientWithoutAge)).rejects.toEqual(
-      new Error("no cd person age")
+  describe("mapGenderCode", () => {
+    test("should return unknown", () => {
+      expect(mapGenderCode(GenderCode.unknown)).toEqual("unknown")
+    })
+
+    const mapGenderCases = [
+      [GenderCode.unknown, "unknown"],
+      [GenderCode.male, "male"],
+      [GenderCode.female, "female"],
+      [GenderCode.notApplicable, undefined],
+    ] as const
+    test.each(mapGenderCases)(
+      "if get %p should return %p",
+      (gender: GenderCode, expected: string | undefined) => {
+        expect(mapGenderCode(gender)).toEqual(expected)
+      }
     )
   })
 })
--- src/lib/process.ts
+++ src/lib/process.ts
@@ -14,37 +14,8 @@ import {
 } from "./model"
 
 const processRiskScore = async (
-  message: Patient
+  patient: Patient
 ): Promise<PatientWithRiskScore> => {
-  if (!message.cdPersonAge) {
-    return Promise.reject(new Error("no cd person age"))
-  }
-
-  const requestBody: ApiRequestBody = {
-    age: message.cdPersonAge,
-    gender: mapGenderCode(message.cdPersonGenderCode),
-    height: message.cdPersonHeightMeasure! / 100,
-    weight: message.cdPersonWeightMeasure,
-    infected_discover_date: dayjs(message.emLaboratoryTestDate).format(
-      "YYYY-MM-DD"
-    ),
-    sym1_chest_tightness: +message.emPatientSymptomsCL14!,
-    rf_chronic_heart_disease: +message.emPatientDiseaseCD8!,
-    rf_cirrhosis: +message.emPatientDiseaseCD12!,
-    sym2_fever: +message.emPatientSymptomsCL2!,
-    sym2_cannot_smell: +message.emPatientSymptomsCL9!,
-    sym2_red_eye: +message.emPatientSymptomsCL10!,
-    fac_dyslipidemia: +message.emPatientDiseaseCD11!,
-    fac_hypertension: +message.emPatientDiseaseCD10!,
-    fac_tuberculosis: +message.emPatientDiseaseCD13!,
-    fac_hiv: +message.emPatientDiseaseCD9!,
-    fac_asthma: +message.emPatientDiseaseCD4!,
-    fac_pregnancy: +message.emPatientPregnancyStatus!,
-    fac_bed_ridden_status: +message.emPatientBedriddenStatus!,
-    fac_diarrhea: +message.emPatientSymptomsCL8!,
-    fac_dyspnea: +message.emPatientSymptomsCL13!,
-  }
-
   const requestHeader = {
     "API-KEY": "",
     "Content-Type": "application/x-www-form-urlencoded",
@@ -52,13 +23,16 @@ const processRiskScore = async (
     "Accept-Encoding": "gzip, deflate, br",
     Connection: "keep-alive",
   } as const
+
+  const requestBody: ApiRequestBody = await buildRequestBody(patient)
+
   const riskScoreResponse = await axios.post<RiskScoreResponse>(
     riskScoreApi.RISK_SCORE_ENDPOINT,
     querystring.stringify(requestBody),
     { headers: requestHeader }
   )
   const patientWithRiskScore: PatientWithRiskScore = {
-    ...message,
+    ...patient,
     riskScore: riskScoreResponse.data,
   }
   return patientWithRiskScore
@@ -66,7 +40,40 @@ const processRiskScore = async (
 
 type NHSOGender = "male" | "female" | "unknown"
 
-const mapGenderCode = (
+const buildRequestBody = async (patient: Patient) => {
+  if (!patient.cdPersonAge) {
+    return Promise.reject(new Error("no cd person age"))
+  }
+
+  const requestBody: ApiRequestBody = {
+    age: patient.cdPersonAge,
+    gender: mapGenderCode(patient.cdPersonGenderCode),
+    height: (patient.cdPersonHeightMeasure ?? 0) / 100,
+    weight: patient.cdPersonWeightMeasure,
+    infected_discover_date: dayjs(patient.emLaboratoryTestDate).format(
+      "YYYY-MM-DD"
+    ),
+    sym1_chest_tightness: +patient.emPatientSymptomsCL14!,
+    rf_chronic_heart_disease: +patient.emPatientDiseaseCD8!,
+    rf_cirrhosis: +patient.emPatientDiseaseCD12!,
+    sym2_fever: +patient.emPatientSymptomsCL2!,
+    sym2_cannot_smell: +patient.emPatientSymptomsCL9!,
+    sym2_red_eye: +patient.emPatientSymptomsCL10!,
+    fac_dyslipidemia: +patient.emPatientDiseaseCD11!,
+    fac_hypertension: +patient.emPatientDiseaseCD10!,
+    fac_tuberculosis: +patient.emPatientDiseaseCD13!,
+    fac_hiv: +patient.emPatientDiseaseCD9!,
+    fac_asthma: +patient.emPatientDiseaseCD4!,
+    fac_pregnancy: +patient.emPatientPregnancyStatus!,
+    fac_bed_ridden_status: +patient.emPatientBedriddenStatus!,
+    fac_diarrhea: +patient.emPatientSymptomsCL8!,
+    fac_dyspnea: +patient.emPatientSymptomsCL13!,
+  }
+
+  return requestBody
+}
+
+export const mapGenderCode = (
   genderCode: GenderCode | undefined
 ): undefined | NHSOGender => {
   if (genderCode === undefined) {
